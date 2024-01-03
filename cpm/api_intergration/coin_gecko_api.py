c1766ce554ce250f524b8cb49570472a82f21626
manasvislj <manasvisaluja2003@gmail.com>
2024-01-03 00:05:40 -0500

    Fixed api integration

--- cpm/api_intergration/coin_gecko_api.py
+++ cpm/api_intergration/coin_gecko_api.py
@@ -4,12 +4,12 @@ class CoinGeckoAPI:
     BASE_URL = 'https://api.coingecko.com/api/v3'
 
     @classmethod
-    def get_token_market_data(cls, token_id):
-        url = f'{cls.BASE_URL}/coins/markets?vs_currency=usd&ids={token_id}'
+    def get_tokens(cls):
+        url = f'{cls.BASE_URL}/coins/list'
         try:
             response = requests.get(url)
             if response.status_code == 200:
-                return response.json()  # Process this data as needed
+                return response.json()  # Process and return token data
             else:
                 # Handle other status codes or errors here
                 return None
